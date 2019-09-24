99c1beadf726cfdda015f069082f1ff48f8c9ef7
sublime <thiago.borges@gamersclub.com.br>
2019-09-24 18:43:21 -0300

    launcher fix

--- trabalho-03/launch.py
+++ trabalho-03/launch.py
@@ -1,12 +1,12 @@
 from subprocess import call
 from random import randint
 
-r = randint(0, 4)
+r = randint(30000, 30004)
 
-for i in range(0, 4):
+for i in range(30000, 30004):
     if i == r:
         call(['gnome-terminal', '-e', 'bash', '--command',
-              'python proc.py 30000 {} 1'.format(str(i))])
+              'python proc.py {} 1'.format(str(i))])
     else:
         call(['gnome-terminal', '-e', 'bash', '--command',
-              'python proc.py 30000 {} 0'.format(str(i))])
+              'python proc.py {} 0'.format(str(i))])
